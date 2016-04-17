a24021bc11fda4f506219927a23bd14bfcd00205
RoboDerpy <roboderpy@gmail.com>
2016-04-17 13:40:49 +0700

    Added dpp_importfpp

--- lua/dpp/sv_misc.lua
+++ lua/dpp/sv_misc.lua
@@ -86,3 +86,69 @@ concommand.Add('dpp_importurs', function(ply, cmd, args)
 		FakePrint(ply, 'To apply, write dpp_importurs 1')
 	end
 end)
+
+local FPP_Blocked = {
+	Gravgun1 = 'Gravgun',
+	Physgun1 = 'Physgun',
+	Toolgun1 = 'Tool',
+	EntityDamage1 = 'Damage',
+}
+
+concommand.Add('dpp_importfpp', function(ply, cmd, args)
+	if IsValid(ply) and not ply:IsSuperAdmin() then return end
+	
+	FakePrint(ply, '[DPP] NOTE: DPP imports FPP lists from database, where DPP is running on!')
+	
+	local isTest = not tobool(args[1])
+	
+	DPP.Query('SELECT * FROM fpp_blockedmodels1', function(data)
+		if not data then return end
+		
+		local count = 0
+		
+		for k, row in pairs(data) do
+			local model = row.model
+			
+			count = count + 1
+			
+			if isTest then
+				FakePrint(ply, string.format('[DPP] Block model: %s', model))
+			else
+				DPP.AddBlockedModel(model)
+			end
+		end
+		
+		if isTest then
+			FakePrint(ply, '[DPP] ----------------- End of model list test. Total: ' .. count)
+			FakePrint(ply, '[DPP] Note: This is a test, to commit changes type dpp_importfpp 1')
+		else
+			DPP.DoEcho({IsValid(ply) and team.GetColor(ply:Team()) or Color(196, 0, 255), (IsValid(ply) and ply:Nick() or 'Console'), Color(200, 200, 200), ' Imported FPP blocked models. Total items imported: ' .. count})
+		end
+	end)
+	
+	DPP.Query('SELECT * FROM fpp_blocked1', function(data)
+		if not data then return end
+		
+		local count = 0
+		
+		for k, row in pairs(data) do
+			local Type = FPP_Blocked[row.var]
+			if not Type then continue end
+			
+			count = count + 1
+			
+			if isTest then
+				FakePrint(ply, string.format('[DPP] Block entity %s from %s touch', row.setting, Type))
+			else
+				DPP['AddBlockedEntity' .. Type](row.setting)
+			end
+		end
+		
+		if isTest then
+			FakePrint(ply, '[DPP] ----------------- End of entity list test Total: ' .. count)
+			FakePrint(ply, '[DPP] Note: This is a test, to commit changes type dpp_importfpp 1')
+		else
+			DPP.DoEcho({IsValid(ply) and team.GetColor(ply:Team()) or Color(196, 0, 255), (IsValid(ply) and ply:Nick() or 'Console'), Color(200, 200, 200), ' Imported FPP blocked entities. Total items imported: ' .. count})
+		end
+	end)
+end)
