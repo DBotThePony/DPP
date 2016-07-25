2bae8ccf90699c21bae0ff5ae2ae2bbcb9ce55b6
DBot <roboderpy@gmail.com>
2016-07-25 18:43:57 +0700

    Procedual model for CPPI

--- lua/dpp/sh_cppi.lua
+++ lua/dpp/sh_cppi.lua
@@ -31,11 +31,13 @@ function CPPI.GetVersion()
 end
 
 function CPPI.GetInterfaceVersion()
-	return 1.3, 'modified'
+	return 1.4, 'unofficial'
 end
 
 local entMeta = FindMetaTable('Entity')
 local plyMeta = FindMetaTable('Player')
+
+--Object Oriented Model
 
 function plyMeta:CPPIGetFriends()
 	return DPP.GetFriendTableCPPI(self)
@@ -110,3 +112,71 @@ end)
 hook.Add('DPP.CanTouch', 'DPP.CPPI', function(ply, ent)
 	return hook.Run('CPPICanTouch', ply, ent)
 end)
+
+--Procedual model
+
+function CPPI.GetFriends(self)
+	return DPP.GetFriendTableCPPI(self)
+end
+
+function CPPI.GetOwner(self)
+	local o = DPP.GetOwner(self)
+	if not IsValid(o) then return nil, nil end
+	
+	return o, o:UniqueID()
+end
+
+if SERVER then
+	function CPPI.SetOwner(ply)
+		DPP.SetOwner(self, ply)
+	end
+	
+	function CPPI.SetOwnerUID(self, uid)
+		local ply = player.GetByUniqueID(uid)
+		if not ply then
+			DPP.SetOwner(self, NULL)
+		else
+			DPP.SetOwner(self, ply)
+		end
+	end
+end
+
+function CPPI.CanDamage(self, ply)
+	return DPP.CanDamage(ply, self) ~= false
+end
+
+function CPPI.CanTool(self, ply, mode)
+	return DPP.CanTool(ply, self, mode) ~= false
+end
+
+function CPPI.Drive(self, ply)
+	return DPP.CanDrive(ply, self) ~= false
+end
+
+function CPPI.CanEditVariable(self, ply, key, val, editor)
+	return DPP.CanEditVariable(self, ply, key, val, editor) ~= false
+end
+
+function CPPI.CanProperty(self, ply, str)
+	return DPP.CanProperty(ply, str, ent) ~= false
+end
+
+function CPPI.CanPhysgun(self, ply)
+	return DPP.CanPhysgun(ply, self) ~= false
+end
+
+function CPPI.CanPickup(self, ply)
+	return DPP.CanGravgun(ply, self) ~= false
+end
+
+function CPPI.CanPunt(self, ply)
+	return DPP.CanGravgunPunt(ply, self) ~= false
+end
+
+function CPPI.CanUse(self, ply)
+	return DPP.PlayerUse(ply, self) ~= false
+end
+
+function CPPI.IsOwned(self)
+	return DPP.IsOwned(self)
+end
